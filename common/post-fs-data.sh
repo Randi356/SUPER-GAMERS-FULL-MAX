c05262681189ee3e7a9c18b24ef3e83514fdc644
Randy wahidin <63757267+Randi356@users.noreply.github.com>
2021-06-09 20:33:09 +0700

    Add files via upload

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -6,12 +6,4 @@
 # if Magisk change its mount point in the future
 MODDIR=${0%/*}
 
-# Mod by Rendy
-swapoff /dev/block/zram0 /dev/null 2>&1
-echo 1 > /sys/block/zram0/reset
-echo 6048M > /sys/block/zram0/disksize
-mkswap /dev/block/zram0 /dev/null 2>&1
-swapon /dev/block/zram0 /dev/null 2>&1
-echo 100 > /proc/sys/vm/swappiness
-
 # This script will be executed in post-fs-data mode
